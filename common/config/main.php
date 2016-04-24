573d0681d346acb316f2584814bb75a8975d7fa6
Khalid <khalid@bawes.net>
2016-04-24 15:57:59 +0300

    Setup authclient for Instagram + Name application

--- common/config/main.php
+++ common/config/main.php
@@ -1,5 +1,6 @@
 <?php
 return [
+    'name' => 'PlugTo',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
@@ -10,8 +11,8 @@ return [
             'clients' => [
                 'instagram' => [
                     'class' => 'kotchuprik\authclient\Instagram',
-                    'consumerKey' => 'a9d7f8aa04ce4dc5be54dcd58d821c08',
-                    'consumerSecret' => '33a094c3460a4fdaaa1673ee4f6462a4',
+                    'clientId' => 'a9d7f8aa04ce4dc5be54dcd58d821c08',
+                    'clientSecret' => '33a094c3460a4fdaaa1673ee4f6462a4',
                 ],
             ],
             // other clients
