6f9569819f9904eb878ce07e7e44cd8bc0652785
Khalid <khalid@bawes.net>
2016-04-24 15:08:47 +0300

    Setup Instagram authclient

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,16 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'instagram' => [
+                    'class' => 'kotchuprik\authclient\Instagram',
+                    'consumerKey' => 'a9d7f8aa04ce4dc5be54dcd58d821c08',
+                    'consumerSecret' => '33a094c3460a4fdaaa1673ee4f6462a4',
+                ],
+            ],
+            // other clients
+        ],
     ],
 ];
