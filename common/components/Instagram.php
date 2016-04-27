ee5494858622ebcec7b07ca234459148e86202f1
Khalid <khalid@bawes.net>
2016-04-27 15:30:17 +0300

    Add comments on Instagram component to guide on next steps

--- common/components/Instagram.php
+++ common/components/Instagram.php
@@ -11,33 +11,28 @@ class Instagram extends \kotchuprik\authclient\Instagram
      * All functions to Interact with Instagram will be listed here
      */
     public function testRandom(){
-        //Now test if this access token can expire, and respond to that as needed
-        /**
-         * - If user logs in for first time, his accesstoken is stored in the user table.
-         * - If access token is found to be expired/invalid (must check every instagram response for invalid access token), user will be logged out and prompted to log back in again. + his old auth records are deleted
-         * - When user logs in with IG + he already has an account, update his access token and create a new auth rule for him.
-         */
-
-        //Test Meta responses, if anything other than code 200 is returned, log an error Yii2 / Maybe Slack?
+        //Test Meta response code and error messages on every request to Instagram API
+        //If anything other than code 200 is returned, log an error Yii2 / Maybe Slack?
         //More info: https://www.instagram.com/developer/endpoints/
-        //Any errors related to the token must log the user out and disables all functionality until he re-enables his token
 
-        
+        //Any errors related to the token must disable all functionality until he re-enables his token
+        //User must login to Instagram section of website to update or refresh his token if the error is a token issue
+
         print_r($this->apiWithToken('35734335.a9d7f8a.1a5d6221613c40b6a1763c9c46fe3bc3' ,
                 'users/self/media/recent',
                 'GET',
                 [
                     'count' => 2,
                 ]));
-        
+
 
         //BAWES ACCESS Token
         //1512951558.a9d7f8a.e6a6122d8a0a486ebb351b25c9f4ad86
         //KHALID ACCESS Token
         //35734335.a9d7f8a.1a5d6221613c40b6a1763c9c46fe3bc3
     }
-    
-    
+
+
     /**
      * Core Functions listed below
      */
