897a54e18e8fc7e50d547f5f874715e9ab824c1e
Khalid <khalid@bawes.net>
2016-05-10 22:50:20 +0300

    Fix comment display

--- backend/views/comment/view.php
+++ backend/views/comment/view.php
@@ -6,13 +6,13 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model common\models\Comment */
 
-$this->title = $model->comment_id;
+$this->title = "Comment by ". $model->comment_by_username;
 $this->params['breadcrumbs'][] = ['label' => 'Comments', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="comment-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::img($model->comment_by_photo) ?> <?= Html::encode($this->title) ?></h1>
 
     <?= DetailView::widget([
         'model' => $model,
@@ -22,12 +22,12 @@ $this->params['breadcrumbs'][] = $this->title;
             'comment_instagram_id',
             'comment_text:ntext',
             'comment_by_username',
-            'comment_by_photo',
+            'comment_by_photo:url',
             'comment_by_id',
             'comment_by_fullname',
             'comment_deleted',
             'comment_deleted_reason:ntext',
-            'comment_datetime',
+            'comment_datetime:datetime',
         ],
     ]) ?>
 
