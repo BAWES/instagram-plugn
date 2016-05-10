7d80e3b7b850a400c433f0c78a993b9eeac6f906
Khalid <khalid@bawes.net>
2016-05-10 22:34:45 +0300

    Adjust media display in backend

--- backend/views/media/view.php
+++ backend/views/media/view.php
@@ -6,31 +6,31 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model common\models\Media */
 
-$this->title = $model->media_id;
+$this->title = "Post by ".$model->user->user_name;
 $this->params['breadcrumbs'][] = ['label' => 'Media', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="media-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::img($model->media_image_thumb); ?> <?= Html::encode($this->title) ?></h1>
 
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
             'media_id',
-            'user_id',
+            'user.user_name',
             'media_instagram_id',
             'media_type',
-            'media_link',
+            'media_link:url',
             'media_num_comments',
             'media_num_likes',
             'media_caption:ntext',
-            'media_image_lowres',
-            'media_image_thumb',
-            'media_image_standard',
-            'media_video_lowres',
-            'media_video_lowbandwidth',
-            'media_video_standard',
+            'media_image_lowres:url',
+            'media_image_thumb:url',
+            'media_image_standard:url',
+            'media_video_lowres:url',
+            'media_video_lowbandwidth:url',
+            'media_video_standard:url',
             'media_location_name',
             'media_location_longitude',
             'media_location_latitude',
