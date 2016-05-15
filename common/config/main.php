3af6c0bb7569f8d52458f38bab368e3c856e194a
Khalid <khalid@bawes.net>
2016-05-15 16:52:36 +0300

    Setup i18n config

--- common/config/main.php
+++ common/config/main.php
@@ -6,5 +6,31 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'assetManager' => [
+            //Link assets -> create symbolic links to assets
+            'linkAssets' => true,
+
+            //append time stamps to assets for cache busting
+            //'appendTimestamp' => true,
+        ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                    'sourceLanguage' => 'en',
+                ],
+                'app' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                    'sourceLanguage' => 'en',
+                ],
+                'yii' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/messages',
+                    'sourceLanguage' => 'en',
+                ],
+            ],
+        ],
     ],
 ];
