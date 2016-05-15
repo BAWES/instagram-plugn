17d02690462cffee1e23050079e5fade3fc228ff
Khalid <khalid@bawes.net>
2016-05-15 17:08:02 +0300

    Setup mailer templates and config

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -11,6 +11,20 @@ return [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            'htmlLayout' => 'layouts/plugn-html',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.sendgrid.net',
+                'username' => 'usernamehere',
+                'password' => 'password!23',
+                'port' => '587',
+                'encryption' => 'tls',
+                'plugins' => [
+                    [
+                        'class' => 'Openbuildings\Swiftmailer\CssInlinerPlugin',
+                    ],
+                ],
+            ],
         ],
         'session' => [ //Use Redis Database for Session Storage
             'class' => 'yii\redis\Session',
