bd92fcf882c7542f3d6a5dfff6146caa8ce24373
Khalid <khalid@bawes.net>
2016-06-25 20:28:46 +0300

    Design setup on IG Panel

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -4,73 +4,162 @@
 /* @var $content string */
 
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use frontend\assets\AppAsset;
+use yii\helpers\Url;
+use frontend\assets\TemplateAsset;
 use common\widgets\Alert;
 
-AppAsset::register($this);
+TemplateAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head lang="<?= Yii::$app->language ?>">
+	<meta charset="<?= Yii::$app->charset ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
     <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
+
+	<title><?= Html::encode($this->title) ?></title>
+
+	<link href="img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
+	<link href="img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
+	<link href="img/favicon.72x72.png" rel="apple-touch-icon" type="image/png" sizes="72x72">
+	<link href="img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
+	<link href="img/favicon.png" rel="icon" type="image/png">
+	<link href="img/favicon.ico" rel="shortcut icon">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
     <?php $this->head() ?>
 </head>
-<body>
+<body class="with-side-menu">
 <?php $this->beginBody() ?>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => 'Plugn.io',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [];
+    <header class="site-header">
+        <div class="container-fluid">
+            <a href="#" class="site-logo">
+                <img class="hidden-md-down" src="<?= Url::to('@web/img/plugn-logo.png') ?>" alt="">
+                <img class="hidden-lg-up" src="<?= Url::to('@web/img/plugn-logo-mob.png') ?>" alt="">
+            </a>
+            <button class="hamburger hamburger--htla">
+                <span>toggle menu</span>
+            </button>
+            <div class="site-header-content">
+                <div class="site-header-content-in">
+                    <div class="site-header-shown">
 
-    if (Yii::$app->user->isGuest) { //Logged Out Menu
-        $menuItems[] = ['label' => 'Home', 'url' => ['/site/index']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else { //Logged In Menu
-        $menuItems[] = ['label' => 'Home', 'url' => ['/site/index']];
-        $menuItems[] = ['label' => 'Agents', 'url' => ['/agent/index']];
-        $menuItems[] = [
-            'label' => 'Logout (' . Yii::$app->user->identity->user_name . ')',
-            'url' => ['/site/logout'],
-            'linkOptions' => ['data-method' => 'post']
-        ];
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
+                        <div class="dropdown dropdown-lang">
+                            <button class="dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="flag-icon flag-icon-us"></span>
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <div class="dropdown-menu-col">
+                                    <a class="dropdown-item" href="#"><span class="flag-icon flag-icon-ru"></span>Русский</a>
+                                    <a class="dropdown-item" href="#"><span class="flag-icon flag-icon-de"></span>Deutsch</a>
+                                    <a class="dropdown-item" href="#"><span class="flag-icon flag-icon-it"></span>Italiano</a>
+                                    <a class="dropdown-item" href="#"><span class="flag-icon flag-icon-es"></span>Español</a>
+                                    <a class="dropdown-item" href="#"><span class="flag-icon flag-icon-pl"></span>Polski</a>
+                                    <a class="dropdown-item" href="#"><span class="flag-icon flag-icon-li"></span>Lietuviu</a>
+                                </div>
+                                <div class="dropdown-menu-col">
+                                    <a class="dropdown-item current" href="#"><span class="flag-icon flag-icon-us"></span>English</a>
+                                    <a class="dropdown-item" href="#"><span class="flag-icon flag-icon-fr"></span>Français</a>
+                                    <a class="dropdown-item" href="#"><span class="flag-icon flag-icon-by"></span>Беларускi</a>
+                                    <a class="dropdown-item" href="#"><span class="flag-icon flag-icon-ua"></span>Українська</a>
+                                    <a class="dropdown-item" href="#"><span class="flag-icon flag-icon-cz"></span>Česky</a>
+                                    <a class="dropdown-item" href="#"><span class="flag-icon flag-icon-ch"></span>中國</a>
+                                </div>
+                            </div>
+                        </div>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
+                        <div class="dropdown user-menu">
+                            <button class="dropdown-toggle" id="dd-user-menu" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img src="<?= Url::to('@web/img/avatar-2-64.png') ?>" alt="">
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dd-user-menu">
+                                <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-user"></span>Profile</a>
+                                <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-cog"></span>Settings</a>
+                                <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-question-sign"></span>Help</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#"><span class="font-icon glyphicon glyphicon-log-out"></span>Logout</a>
+                            </div>
+                        </div>
+
+                    </div><!--.site-header-shown-->
+
+                </div><!--site-header-content-in-->
+            </div><!--.site-header-content-->
+        </div><!--.container-fluid-->
+    </header><!--.site-header-->
+
+    <div class="mobile-menu-left-overlay"></div>
+    <nav class="side-menu">
+        <div class="side-menu-avatar">
+            <div class="avatar-preview avatar-preview-100">
+                <img src="<?= Url::to('@web/img/instagram-account/account2.jpg') ?>" alt="">
+                <span style='color:#343434; font-weight:600'>@bawestech</span>
+            </div>
+        </div>
+        <ul class="side-menu-list">
+            <li class="blue opened">
+                <a href="pn-ig-layout.html">
+                    <i class="font-icon font-icon-users"></i>
+                    <span class="lbl">Agents</span>
+                </a>
+            </li>
+            <li class="pink-red">
+                <a href="pn-ig-activity.html">
+                    <i class="font-icon font-icon-zigzag"></i>
+                    <span class="lbl">Activity</span>
+                </a>
+            </li>
+            <li class="aquamarine">
+                <a href="pn-ig-billing.html">
+                    <i class="font-icon font-icon-build"></i>
+                    <span class="lbl">Billing</span>
+                </a>
+            </li>
+            <li class="blue-sky">
+                <a href="pn-ig-help.html">
+                    <i class="font-icon font-icon-question"></i>
+                    <span class="lbl">Help</span>
+                </a>
+            </li>
+        </ul>
+        <section>
+            <header class="side-menu-title">Agent Portal</header>
+            <ul class="side-menu-list">
+                <li>
+                    <a href="pn-agent-layout.html" target='_blank'>
+                        <i class="font-icon font-icon-user"></i>
+                        Switch to agent portal
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <header class="side-menu-title">Mobile Apps [Soon]</header>
+            <ul class="side-menu-list">
+                <li>
+                    <a href="#" style='padding-left:22px'>
+                        <img src="<?= Url::to('@web/img/applecomingsoon.png') ?>" alt="Soon on App Store" style="width:88px">
+                        <img src="<?= Url::to('@web/img/playcomingsoon.png') ?>" alt="Soon on Play Store" style="width:88px">
+                    </a>
+                </li>
+            </ul>
+        </section>
+    </nav><!--.side-menu-->
+
+    <div class="page-content">
+
         <?= $content ?>
-    </div>
-</div>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; <?= date('Y') ?></p>
 
-        <p class="pull-right">A weekend project</p>
-    </div>
-</footer>
+	</div><!--.page-content-->
 
 <?php $this->endBody() ?>
 </body>
