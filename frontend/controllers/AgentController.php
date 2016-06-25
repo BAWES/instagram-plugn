0aa4e8f2e493d6a514c8a759c9978c912a558c7e
Khalid <khalid@bawes.net>
2016-06-26 01:53:29 +0300

    Remove unused actions

--- frontend/controllers/AgentController.php
+++ frontend/controllers/AgentController.php
@@ -64,26 +64,6 @@ class AgentController extends Controller
         ]);
     }
 
-
-    /**
-     * Creates a new AgentAssignment model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new AgentAssignment();
-        $model->user_id = Yii::$app->user->identity->user_id;
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->assignment_id]);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
-    }
-
     /**
      * Deletes an existing AgentAssignment model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
