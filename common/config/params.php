bcb182d041a92f23e54c4b0f2ba211175bacd362
Khalid <khalid@bawes.net>
2016-07-19 10:07:33 +0300

    Update params for production version

--- common/config/params.php
+++ common/config/params.php
@@ -3,7 +3,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'instagram.numberOfPastPostsToCrawl' => 20,
-    'instagram.endpointHourlyRateLimit' => 30, //30 per hour for sandbox, 60 per hour for live
+    'instagram.endpointHourlyRateLimit' => 60, //30 per hour for sandbox, 60 per hour for live
     'user.passwordResetTokenExpire' => 3600,
     'user.rememberMeDuration' => 3600 * 24 * 30,
 ];
