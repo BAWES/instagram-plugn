0c3de7aad17475ed0a0556cc472b6143d796f079
Khalid <khalid@bawes.net>
2016-07-24 18:52:27 +0300

    Install livechat

--- agent/views/layouts/main.php
+++ agent/views/layouts/main.php
@@ -23,6 +23,16 @@ ga('send', 'pageview');
 $this->registerJs($analytics);
 
 
+//Live Chat by Intercom.io
+$liveChat = "
+window.intercomSettings = {
+	app_id: "a25z3ilz"
+};
+
+(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/a25z3ilz';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+";
+$this->registerJs($liveChat);
+
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
