6f8cf27f26a5a083f58670b5c33f858c93cf0914
Khalid <khalid@bawes.net>
2016-08-12 22:57:00 +0300

    Allow agent to change email preference

--- agent/views/email/index.php
+++ agent/views/email/index.php
@@ -1,9 +1,12 @@
 <?php
 
 /* @var $this yii\web\View */
+/* @var $agentModel common\models\Agent */
 
 use yii\helpers\Html;
 use yii\helpers\Url;
+use yii\bootstrap\ActiveForm;
+use common\models\Agent;
 
 $this->title = "Email Preferences";
 ?>
@@ -21,39 +24,48 @@ $this->title = "Email Preferences";
 		<div class="card-block" style="padding-top:0">
 			<div class="row m-t-lg">
 
+				<?php $form = ActiveForm::begin([
+	                'id' => 'notification-preference-form',
+	                'errorCssClass' => 'form-group-error',
+	            ]); ?>
 
-
-				<div class="col-sm-6">
-					<div class="checkbox-detailed">
-						<input type="radio" name="detailed" id="check-det-2" checked/>
-						<label for="check-det-2">
-						<span class="checkbox-detailed-tbl">
-							<span class="checkbox-detailed-cell">
-								<span class="checkbox-detailed-title">Once a day</span>
-								Every 24 hours
+					<div class="col-sm-6">
+						<div class="checkbox-detailed">
+							<input type="radio" name="notif-preference" id="notif-once-a-day" value="<?= Agent::PREF_EMAIL_DAILY ?>"
+							<?= $agentModel->agent_email_preference==Agent::PREF_EMAIL_DAILY?"checked":"" ?>/>
+							<label for="notif-once-a-day">
+							<span class="checkbox-detailed-tbl">
+								<span class="checkbox-detailed-cell">
+									<span class="checkbox-detailed-title">Once a day</span>
+									Every 24 hours
+								</span>
 							</span>
-						</span>
-						</label>
+							</label>
+						</div>
 					</div>
-				</div>
 
-				<div class="col-sm-6">
-					<div class="checkbox-detailed">
-						<input type="radio" name="detailed" id="check-det-1"/>
-						<label for="check-det-1">
-						<span class="checkbox-detailed-tbl">
-							<span class="checkbox-detailed-cell">
-								<span class="checkbox-detailed-title">Off</span>
-								Don't send me comment notifications
+					<div class="col-sm-6">
+						<div class="checkbox-detailed">
+							<input type="radio" name="notif-preference" id="notif-off" value="<?= Agent::PREF_EMAIL_OFF ?>"
+							<?= $agentModel->agent_email_preference==Agent::PREF_EMAIL_OFF?"checked":"" ?>/>
+							<label for="notif-off">
+							<span class="checkbox-detailed-tbl">
+								<span class="checkbox-detailed-cell">
+									<span class="checkbox-detailed-title">Off</span>
+									Don't send me comment notifications
+								</span>
 							</span>
-						</span>
-						</label>
+							</label>
+						</div>
 					</div>
-				</div>
 
-				<div class='col-xs-12'>
-					<input type='submit' class='btn btn-block btn-primary' value='Save'/>
-				</div>
+					<div class='col-xs-12'>
+						<?= Html::submitButton('Save', [
+							'class' => 'btn btn-block btn-primary',
+							'name' => 'save-button']) ?>
+					</div>
+
+				<?php ActiveForm::end(); ?>
 
 			</div><!--.row-->
 		</div>
