77396a872f9ef3b7348778d0afc5c0ed685b83b8
Khalid <khalid@bawes.net>
2016-10-19 12:29:03 +0300

    Allow v1 config to load from config.php file

--- agent/api/v1/Module.php
+++ agent/api/v1/Module.php
@@ -19,6 +19,9 @@ class Module extends \yii\base\Module
     {
         parent::init();
 
+        // initialize the module with the configuration loaded from config.php
+        \Yii::configure($this, require(__DIR__ . '/config.php'));
+
         // custom initialization code goes here
     }
 }
