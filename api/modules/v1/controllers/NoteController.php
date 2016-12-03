e97ef1042d07cae217e57453a779a3981ddd396c
Khalid <khalid@bawes.net>
2016-12-03 23:33:04 +0300

    Create initial note api controller

---
+++ api/modules/v1/controllers/NoteController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use Yii;
+use yii\rest\Controller;
+
+/**
+ * Notes created by agent about a specific user (CRM)
+ */
+class NoteController extends Controller
+{
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        // remove authentication filter for cors to work
+        unset($behaviors['authenticator']);
+
+        // Allow XHR Requests from our different subdomains and dev machines
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                'Origin' => Yii::$app->params['allowedOrigins'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => null,
+                'Access-Control-Max-Age' => 86400,
+                'Access-Control-Expose-Headers' => [],
+            ],
+        ];
+
+        // Bearer Auth checks for Authorize: Bearer <Token> header to login the user
+        $behaviors['authenticator'] = [
+            'class' => \yii\filters\auth\HttpBearerAuth::className(),
+        ];
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['options'];
+
+        return $behaviors;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        $actions = parent::actions();
+        $actions['options'] = [
+            'class' => 'yii\rest\OptionsAction',
+            // optional:
+            'collectionOptions' => ['GET', 'POST', 'HEAD', 'OPTIONS'],
+            'resourceOptions' => ['GET', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+        ];
+        return $actions;
+    }
+
+    /**
+     * Get all notes made for this user on this account
+     * @param  [integer] $accountId
+     * @param  [string] $username
+     * @return array
+     */
+     public function actionGetNotes($accountId, $username)
+     {
+         // Get Instagram account from Account Manager component
+         $instagramAccount = Yii::$app->accountManager->getManagedAccount($accountId);
+
+         //Get All Activities with the User that activity was made on
+         // $activities = Activity::find()
+         //                 ->with(['user', 'agent'])
+         //                 ->where(['user_id' => $instagramAccount->user_id])
+         //                 ->orderBy('activity_datetime DESC')
+         //                 ->all();
+         //
+         // return $activities;
+
+         // Check SQL Query Count and Duration
+         return Yii::getLogger()->getDbProfiling();
+     }
+
+     /**
+      * Create a note
+      */
+     public function actionCreateNote()
+     {
+         // Get the passed params
+         $accountId = Yii::$app->request->getBodyParam("accountId");
+         $noteAboutUsername = Yii::$app->request->getBodyParam("note_about_username");
+         $noteTitle = Yii::$app->request->getBodyParam("note_title");
+         $noteText = Yii::$app->request->getBodyParam("note_text");
+
+         // Get Instagram account from Account Manager component
+         $instagramAccount = Yii::$app->accountManager->getManagedAccount($accountId);
+
+         //Get All Activities with the User that activity was made on
+         // $activities = Activity::find()
+         //                 ->with(['user', 'agent'])
+         //                 ->where(['user_id' => $instagramAccount->user_id])
+         //                 ->orderBy('activity_datetime DESC')
+         //                 ->all();
+         //
+         // return $activities;
+
+         // Check SQL Query Count and Duration
+         return Yii::getLogger()->getDbProfiling();
+     }
+
+     /**
+      * Update a note
+      */
+     public function actionUpdateNote()
+     {
+         // Get the passed params
+         $noteId = Yii::$app->request->getBodyParam("noteId");
+         $accountId = Yii::$app->request->getBodyParam("accountId");
+         $noteTitle = Yii::$app->request->getBodyParam("note_title");
+         $noteText = Yii::$app->request->getBodyParam("note_text");
+
+         // Get Instagram account from Account Manager component
+         $instagramAccount = Yii::$app->accountManager->getManagedAccount($accountId);
+
+         //Get All Activities with the User that activity was made on
+         // $activities = Activity::find()
+         //                 ->with(['user', 'agent'])
+         //                 ->where(['user_id' => $instagramAccount->user_id])
+         //                 ->orderBy('activity_datetime DESC')
+         //                 ->all();
+         //
+         // return $activities;
+
+         // Check SQL Query Count and Duration
+         return Yii::getLogger()->getDbProfiling();
+     }
+
+     /**
+      * Delete a note
+      */
+     public function actionDeleteNote()
+     {
+         // Get the passed params
+         $noteId = Yii::$app->request->getBodyParam("noteId");
+         $accountId = Yii::$app->request->getBodyParam("accountId");
+
+         // Get Instagram account from Account Manager component
+         $instagramAccount = Yii::$app->accountManager->getManagedAccount($accountId);
+
+         //Get All Activities with the User that activity was made on
+         // $activities = Activity::find()
+         //                 ->with(['user', 'agent'])
+         //                 ->where(['user_id' => $instagramAccount->user_id])
+         //                 ->orderBy('activity_datetime DESC')
+         //                 ->all();
+         //
+         // return $activities;
+
+         // Check SQL Query Count and Duration
+         return Yii::getLogger()->getDbProfiling();
+     }
+}
