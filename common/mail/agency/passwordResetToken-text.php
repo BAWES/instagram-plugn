a5d1552add3b2e7ee96f03a8efc3af20a49cf7db
Khalid <khalid@bawes.net>
2016-12-28 19:42:49 +0300

    Add email templates for agency

---
+++ common/mail/agency/passwordResetToken-text.php
@@ -0,0 +1,10 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $agency common\models\Agency */
+?>
+Hello <?= $agency->agency_fullname ?>,
+
+Follow the link below to reset your password:
+
+<?= $resetLink ?>
