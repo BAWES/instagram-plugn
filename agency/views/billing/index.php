6c6360b90cb9543697a853df7b0909c94e31e5b4
Khalid <khalid@bawes.net>
2017-01-03 18:31:46 +0300

    Initial pricing option design

--- agency/views/billing/index.php
+++ agency/views/billing/index.php
@@ -28,64 +28,27 @@ $this->title = 'Billing';
 		<!-- Pricing Tables -->
 		<div class="box-typical-center">
 			<div class="box-typical-center-in prices-page">
-				<header class="prices-page-title">Affordable pricing. No long term contract. No per-user fees.</header>
-				<p class="prices-page-subtitle">Try free for 30 days whith no obligation.</p>
+				<header class="prices-page-title">Affordable pricing. No long term contract.</header>
+				<p class="prices-page-subtitle">Try free for 14 days with no obligation.</p>
+
 				<article class="price-card">
 					<header class="price-card-header">Enterprise</header>
 					<div class="price-card-body">
 						<div class="price-card-amount">$499</div>
 						<div class="price-card-amount-lbl">per month</div>
 						<ul class="price-card-list">
-							<li><i class="font-icon font-icon-ok"></i>Up to 5000 bins</li>
-							<li><i class="font-icon font-icon-ok"></i>UNLIMITED users</li>
+							<li style='text-align:center'>
+								up to <span style='font-size:1.1em; font-weight:bold'>5</span> Instagram accounts
+							</li>
 						</ul>
 						<div class="clear"></div>
-						<a href="<?= Url::to(['billing/setup']) ?>" class="btn btn-rounded">Start your trial</a>
+						<a href="<?= Url::to(['billing/setup']) ?>" class="btn btn-rounded">Buy now</a>
 					</div>
 				</article>
-				<article class="price-card">
-					<header class="price-card-header">Medium</header>
-					<div class="price-card-body">
-						<div class="price-card-amount">$299</div>
-						<div class="price-card-amount-lbl">per month</div>
-						<div class="price-card-label">Our most popular plan</div>
-						<ul class="price-card-list">
-							<li><i class="font-icon font-icon-ok"></i>Up to 5000 bins</li>
-							<li><i class="font-icon font-icon-ok"></i>UNLIMITED users</li>
-						</ul>
-						<div class="clear"></div>
-						<a href="<?= Url::to(['billing/setup']) ?>" class="btn btn-rounded">Start your trial</a>
-					</div>
-				</article>
-				<article class="price-card">
-					<header class="price-card-header">Small</header>
-					<div class="price-card-body">
-						<div class="price-card-amount">$99</div>
-						<div class="price-card-amount-lbl">per month</div>
-						<ul class="price-card-list">
-							<li><i class="font-icon font-icon-ok"></i>Up to 5000 bins</li>
-							<li><i class="font-icon font-icon-ok"></i>UNLIMITED users</li>
-						</ul>
-						<div class="clear"></div>
-						<a href="<?= Url::to(['billing/setup']) ?>" class="btn btn-rounded">Start your trial</a>
-					</div>
-				</article>
-				<article class="price-card">
-					<header class="price-card-header">Basic</header>
-					<div class="price-card-body">
-						<div class="price-card-amount">$49</div>
-						<div class="price-card-amount-lbl">per month</div>
-						<ul class="price-card-list">
-							<li><i class="font-icon font-icon-ok"></i>Up to 5000 bins</li>
-							<li><i class="font-icon font-icon-ok"></i>UNLIMITED users</li>
-						</ul>
-						<div class="clear"></div>
-						<a href="<?= Url::to(['billing/setup']) ?>" class="btn btn-rounded">Start your trial</a>
-					</div>
-				</article>
+
 				<div class="prices-page-bottom">
-					<p>Larger plans are available on request.</p>
-					<p>Save 10% by signing up for a year in advance. <a href="#">Contact us</a> for more information.</p>
+					<p>Larger plans available upon request.</p>
+					<p><a href="https://plugn.io/contact/">Contact us</a> for more information.</p>
 				</div>
 			</div>
 		</div>
