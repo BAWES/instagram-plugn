e5a81bc1604cfe0c2787c1071d8efb60e6399e38
Khalid <khalid@bawes.net>
2017-01-07 17:27:34 +0300

    Update user activity display

--- backend/views/activity/view.php
+++ backend/views/activity/view.php
@@ -1,28 +1,23 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\DetailView;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Activity */
 
-$this->title = $model->activity_id;
+$this->title = "Activity #".$model->activity_id;
 $this->params['breadcrumbs'][] = ['label' => 'Activities', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="activity-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h2><?= Html::encode($model->activity_detail) ?></h2>
+    <h4><?= Yii::$app->formatter->asDateTime($model->activity_datetime, "long") ?></h4>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'activity_id',
-            'user_id',
-            'agent_id',
-            'activity_detail:ntext',
-            'activity_datetime',
-        ],
-    ]) ?>
+
+    <a target='_blank' href='<?= Url::to(['agent/view', 'id' => $model->agent->agent_id]) ?>' class='btn btn-lg btn-primary'>Go to Agent (<?= $model->agent->agent_name ?>)</a>
+
+    <a target='_blank' href='<?= Url::to(['instagram-user/view', 'id' => $model->user->user_id]) ?>' class='btn btn-lg btn-primary'>Go to Account (@<?=$model->user->user_name ?>)</a>
 
 </div>
