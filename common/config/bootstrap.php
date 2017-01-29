465c93fc2dd895a584eba26c859f73e3e7b90abd
Khalid <khalid@bawes.net>
2017-01-29 11:37:43 +0300

    Delete agency portal

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 Yii::setAlias('common', dirname(__DIR__));
-Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('agency', dirname(dirname(__DIR__)) . '/agency');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('agent', dirname(dirname(__DIR__)) . '/agent');
 Yii::setAlias('api', dirname(dirname(__DIR__)) . '/api');
