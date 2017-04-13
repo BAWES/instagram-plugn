ae8f17e404f2a79d4dde086199a0e341a6264ad2
Khalid <khalid@bawes.net>
2017-04-13 22:00:52 +0300

    Coupon table and crud implemented

---
+++ backend/views/coupon/_form.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use dosamigos\datepicker\DatePicker;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Coupon */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="coupon-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'coupon_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'coupon_user_limit')->textInput() ?>
+
+    <?= $form->field($model, 'coupon_expires_at')->widget(
+        DatePicker::className(), [
+            // inline too, not bad
+             'inline' => true,
+             // modify template for custom rendering
+            'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+            'clientOptions' => [
+                'autoclose' => true,
+                'format' => 'yyyy-mm-dd'
+            ]
+    ]);?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ common/models/CouponSearch.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Coupon;
+
+/**
+ * CouponSearch represents the model behind the search form about `common\models\Coupon`.
+ */
+class CouponSearch extends Coupon
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['coupon_id', 'coupon_user_limit'], 'integer'],
+            [['coupon_name', 'coupon_expires_at', 'coupon_created_at', 'coupon_updated_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Coupon::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'coupon_id' => $this->coupon_id,
+            'coupon_user_limit' => $this->coupon_user_limit,
+            'coupon_expires_at' => $this->coupon_expires_at,
+            'coupon_created_at' => $this->coupon_created_at,
+            'coupon_updated_at' => $this->coupon_updated_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'coupon_name', $this->coupon_name]);
+
+        return $dataProvider;
+    }
+}
